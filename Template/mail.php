a962558d8df31f340a10ad1a7e24732a7b06e228
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-27 12:39:18 +0200

    travail sur le formulaire de contact

---
+++ Template/mail.php
@@ -0,0 +1,9 @@
+    <?php
+
+    require("La%20P'tite%20Mine%20de%20Sab.html");
+
+    $retour = mail("s.mouginot@yahoo.fr","Nom", $_POST['message'], "From: E-mail");
+    if ($retour)
+        echo '<p>Votre message a bien été envoyé.</p>';
+
+    ?>
