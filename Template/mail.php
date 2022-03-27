4431d70774abe92ce3d5910ed9e308e14940ae16
SabrinaMouginot <s.mouginot@yahoo.fr>
2022-03-27 17:59:05 +0200

    réflexion sur le responsive qui n'est pas encore fait. A faire de toute urgence

--- Template/mail.php
+++ Template/mail.php
@@ -1,5 +1,5 @@
-<?php
+<!-- 
     $retour = mail("s.mouginot@yahoo.fr","Nom", $_POST['message'], "From: E-mail");
     if ($retour)
         echo '<p>Votre message a bien été envoyé.</p>';
-?>
+?> -->
